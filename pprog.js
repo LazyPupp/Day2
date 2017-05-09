71b88ff02add2459556a28bab86621c724328bdc
Saule BN <beyonce@thinkful.com>
2017-05-09 12:57:45 -0400

    higher order functions

--- pprog.js
+++ pprog.js
@@ -1,6 +1,6 @@
 function average(numbers){
- let sum =0;
-	numbers.forEach(function(i){
+    let sum =0;
+    numbers.forEach(function(i){
         sum+=i;
     });
     return sum/numbers.length;
@@ -12,7 +12,7 @@ average([1,2,3]);
 
 function average(numbers){
     var sum=0;
-   let i = 0;
+    let i = 0;
     while (i<numbers.length){
         sum+=numbers[i];
         i++;
@@ -24,27 +24,27 @@ average([1,2,3]);
 
 
 function max(myNumbers){
-	let a=Number.NEGATIVE_INFINITY;
-	let i = 0;
-	while(i<myNumbers.length){
-		if(myNumbers[i]> a){
-			a = myNumbers[i];
-		}
-		i++;
-	}
-	return a;
+    let a=Number.NEGATIVE_INFINITY;
+    let i = 0;
+    while(i<myNumbers.length){
+        if(myNumbers[i]> a){
+        a = myNumbers[i];
+    }
+        i++;
+    }
+    return a;
 }
 
 function min(myNumbers){
-	let a = Number.POSITIVE_INFINITY;
-	let i =0;
-	while(i<myNumbers.length){
-		if(myNumbers[i]< a){
-			a = myNumbers[i];
-		}
-		i++;
-	}
-	return a;
+    let a = Number.POSITIVE_INFINITY;
+    let i =0;
+    while(i<myNumbers.length){
+        if(myNumbers[i]< a){
+        a = myNumbers[i];
+    }
+        i++;
+    }
+    return a;
 }
 
 arr = [1,2,-20,100];
@@ -52,45 +52,61 @@ console.log(max(arr)); //=> 100
 console.log(min(arr)); //=> -20
 
 function fizzBuzz(countTo){
-	for(let i = 1; i <= countTo;i++){
-		switch(i%15){
-			case 0:
-				console.log("fizzbuzz");
-				break;
-			default:
-				switch(i%5){
-					case 0: 
-						console.log("buzz");
-						break;
-					default:
-						switch(i%3){
-							case 0:
-								console.log("fizz");
-								break;
-							default:
-								console.log(i);
-						}
-				}
+    for(let i = 1; i <= countTo;i++){
+        switch(i%15){
+    case 0:
+        console.log('fizzbuzz');
+        break;
+    default:
+        switch(i%5){
+    case 0: 
+        console.log('buzz');
+        break;
+    default:
+        switch(i%3){
+    case 0:
+        console.log('fizz');
+        break;
+    default:
+        console.log(i);
+    }
+    }
 
-		}
-	}
+    }
+    }
 }
 
 function fizzBuzz(countTo){
-	for(let i = 1; i <= countTo; i++){
-		switch(true){
-			case (i%15===0):
-				console.log("fizzbuzz");
-				break;
-			case (i%5 ===0):
-				console.log("buzz");
-				break;
-			case (i%3===0):
-				console.log("fizz");
-				break;
-			default:
-				console.log(i);
-		}
-	}
+    for(let i = 1; i <= countTo; i++){
+        switch(true){
+    case (i%15===0):
+        console.log('fizzbuzz');
+        break;
+    case (i%5 ===0):
+        console.log('buzz');
+        break;
+    case (i%3===0):
+        console.log('fizz');
+        break;
+    default:
+        console.log(i);
+    }
+    }
 }
 fizzBuzz(15);
+
+function repeat (fn,n){
+    for (let i=1;i<=n;i++){
+        fn();
+    }
+}
+function hello(){
+	console.log("Hello world!");
+
+}
+function goodbye(){
+	console.log("Goodbye world!");
+}
+
+repeat(hello,5);
+repeat(goodbye, 5);
