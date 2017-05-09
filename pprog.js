b1206b8bd8c7e8b4ddf81adf36a5069dac0621b8
Saule BN <beyonce@thinkful.com>
2017-05-09 16:06:09 -0400

    hazard alert

--- pprog.js
+++ pprog.js
@@ -162,3 +162,39 @@ function filter(arr,fn) {
 }
 
 var filter = (arr,fn)=> arr.forEach(str=>const newArray =[]; fn(str)?newArray.push(str):);
+
+//////////////////////////////HAZARD ALERT/////////////////////////////////////////////
+function hazardWarningCreator (typeOfWarning){
+    let warningCounter = 0;
+    return function(location){
+        warningCounter ++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        if (warningCounter === 1){
+            console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time today!`)
+        } 
+        else {console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} times today!`)}
+        //console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+    }
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const floodWarning = hazardWarningCreator('Flood Alert!');
+const heatWarning = hazardWarningCreator('Heat Warning!');
+
+rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+rocksWarning('Centinela Ave and Olympic Blvd');
+// => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// => The Rocks on the Road hazard has triggered 2 time(s) today!
+
+floodWarning('VA,Burke');
+floodWarning('VA,Burke');
+floodWarning('Fairfax, VA');
+
+heatWarning('Axa, AZ');
+heatWarning('Tuscano, AZ');
+heatWarning('Burke, AZ');
+
+//Bonus credit! Can you figure out how to log out "time" for a value of 1 and "times" for a value of 0 or > 2?
